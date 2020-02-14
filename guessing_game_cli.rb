5f1b2203ad3ffd21b9989a558e307e3fc656dbdc
Gene Campbell III <gcampbell391@gmail.com>
2020-02-14 01:36:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
  computer_number = rand(1...6)
  if input == "exit"
    puts "Goodbye!"
- elsif input.to_i == computer_number
+ elsif input.to_i - 1 == computer_number
   return "You guessed the correct number!"
  else
    pp input
