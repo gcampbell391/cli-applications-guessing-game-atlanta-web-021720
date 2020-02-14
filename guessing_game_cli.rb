594c2c5ee7eb5331c40f91a95a4d3002316bac84
Gene Campbell III <gcampbell391@gmail.com>
2020-02-14 01:03:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,14 @@
 def run_guessing_game
  random_number = rand(1...6)
- 
-
+ puts "Guess a number between 1 and 6"
+ user_input = get.chomp
+ if user_input == random_number
+   return "You guess the correct number!"
+ elsif user_input == "exit"
+  return "Goodbye!"
+ else 
+   return "Sorry! The computer guessed #{random_number}"
+ end 
 end 
 
 
