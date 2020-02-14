666ab798190d7e24388ebc592eafc9a23e25f900
Gene Campbell III <gcampbell391@gmail.com>
2020-02-14 01:10:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,5 +1,6 @@
 def run_guessing_game
  random_number = rand(1...6)
+ p "Guess a number 1 to 6"
  user_input = gets.chomp 
  if user_input == random_number
    return "You guess the correct number!"
